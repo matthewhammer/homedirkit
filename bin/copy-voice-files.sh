0d5b2c8e5ec4136f8b8376dc385f6f383f235214
Matthew Hammer <matthew.hammer@gmail.com>
2021-07-12 17:40:55 -0600

    fixes

--- bin/copy-voice-files.sh
+++ bin/copy-voice-files.sh
@@ -13,17 +13,23 @@ sources=(
     /Volumes/IC\ RECORDER/REC_FILE/FOLDER01/
     /Volumes/IC\ RECORDER\ 1/REC_FILE/FOLDER01/
 )
-
+toDelete=()
 for index in ${!targets[*]}; do 
     target=${targets[$index]}
     source=${sources[$index]}
     echo "copy $target <== $source ..."
-    if [ -e "$source" ]; then
-        path="$targRoot/$date/$target"
+    if [ -e "$source/"* ]; then
+        path="$targRoot/$date-sony-ic-recorder/$target"
         mkdir -p $path
-        cp -v /Volumes/IC\ RECORDER/REC_FILE/FOLDER01/* $path
+        cp -v "$source/"* $path
         echo "Done."
+        toDelete+=("\"$source\"""*")
     else
         echo "  Warn: Source does not exist; skipping..."
     fi
 done
+
+echo "Notice on next steps:"
+echo "  The recorder(s) memories retain their copies."
+echo "  Clear the recorder(s) via"
+echo "    rm -r ${toDelete[*]}"
